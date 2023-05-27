52845459dfab6903ac9d7079000380cdb799475d
Anu Sihv <anu.sihv@gmail.com>
2023-05-27 09:56:17 +0300

    magic methods

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
 <?php
 
-class Job {
-    private $logger;
-    public function __construct(Logger $logger) {
-        $this->logger = $logger;
+class Box {
+    public function __construct($num, $color)
+    {
+        echo "object was created\n";
     }
-    public function task(){        
-        for ($i=0;$i<10;$i++){
-            $this->logger->log($i);
-        }
+
+    public function __get($name) {
+        echo "$name is accessed\n";
+        return 'hello';
     }
-}
 
-interface Logger {
-    public function log($message);
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message){
-        echo $message . "\n";
+    public function __set($name, $value) {
+        echo "$name is given value $value\n";
     }
-}
 
+    public function __call($name, $params) {
+        echo "$name is called with \n";
+        var_dump($params);
+    }
 
+    public function __invoke(...$params) {
+        echo "object is used like a function\n";
+        var_dump($params);
+    }
 
+    public function __destruct()
+    {
+        echo "object was destroyed\n";
+    }
 
-
-
-
-
-
-
-
-class NothingLogger implements Logger{
-    public function log($message){
-        
+    public function __toString() {
+        return "bla bla to string\n";
     }
 }
 
-
-$logger = new ConsoleLogger();
-$job = new Job($logger);
-$job->task();
+$box1 = new Box(1, 'color');
+echo $box1->color;
+$box1->material = 'metal';
+$box1->nothing(1, 'hello');
+$box1(1, 'hello');
+$box = 1;
+$box2 = new Box(2, 'dfdf');
+echo $box1;
