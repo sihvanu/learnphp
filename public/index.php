bd08cf93cd9c8ce96446685311221ddf101f9a67
Anu Sihv <anu.sihv@gmail.com>
2023-06-10 13:03:02 +0300

    add autoload

--- public/index.php
+++ public/index.php
@@ -1,13 +1,28 @@
 <?php
 
-switch($_SERVER['REQUEST_URI']){
-    case '/':
-        require 'views/index.php';
-        break;
-    case '/about':
-        require 'views/about.php';
-        break;
-    default:
-        echo '404';
-        break;
-}
+// require_once __DIR__ . '/../src/Router.php';
+
+spl_autoload_register(function ($class) {
+
+    $class = substr($class, strlen(('App\\')));
+
+    require_once __DIR__ . "/../src/$class.php";
+});
+
+use App\Controllers\PublicController as Controller;
+
+$router = new App\Router();
+$dog = new App\Dog();
+$controller = new PublicController();
+
+// switch($_SERVER['REQUEST_URI']){
+//     case '/':
+//         require 'views/index.php';
+//         break;
+//     case '/about':
+//         require 'views/about.php';
+//         break;
+//     default:
+//         echo '404';
+//         break;
+// }
