e24a12bf2c9635678c245c8d6c95e7293f5c70b2
Anu Sihv <anu.sihv@gmail.com>
2023-06-10 15:39:49 +0300

    add database example

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -2,8 +2,27 @@
 
 namespace App\Controllers;
 
+use PDO;
+use PDOException;
+
 class PublicController {
     public function index() {
+        try {
+            $conn = new PDO("sqlite:database.sqlite");
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $stmt = $conn->prepare("SELECT * FROM posts");
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            dump($result);
+          } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+          }
+          die;
         $name = 'Anu';
         view('index', compact('name'));
     }
