9f6ef9f4f0a8c3ff145bf6323680d14b767c5c49
Anu Sihv <anu.sihv@gmail.com>
2023-06-10 13:14:25 +0300

    add composer autoload and vardumper package

--- public/index.php
+++ public/index.php
@@ -2,18 +2,14 @@
 
 // require_once __DIR__ . '/../src/Router.php';
 
-spl_autoload_register(function ($class) {
-
-    $class = substr($class, strlen(('App\\')));
-
-    require_once __DIR__ . "/../src/$class.php";
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\PublicController as Controller;
 
 $router = new App\Router();
 $dog = new App\Dog();
-$controller = new PublicController();
+$controller = new Controller();
+dump($controller);
 
 // switch($_SERVER['REQUEST_URI']){
 //     case '/':
